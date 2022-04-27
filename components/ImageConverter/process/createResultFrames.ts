fdac4d1e3653f2267690d54fefbae166f2536185
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-04-27 11:18:20 -0400

    forgor 💀 to add one of the twerk frames 💀

--- components/ImageConverter/process/createResultFrames.ts
+++ components/ImageConverter/process/createResultFrames.ts
@@ -5,7 +5,7 @@ const createResultFrames = (amoguses: ImageData[][][], { resolution }: Conversio
 		(resolve) => {
 			const finalWidth = resolution * amoguses[0].length;
 			const finalHeight = resolution * amoguses.length;
-			const frames = Array.from(Array(5).keys()).map(() => {
+			const frames = Array.from(Array(6).keys()).map(() => {
 				const canvas = document.createElement('canvas');
 				const ctx = canvas.getContext('2d');
 				if (!ctx) throw new Error('No context found on canvas.');
