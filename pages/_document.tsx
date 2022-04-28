a4ba3863457b663693c2b89a523d90c096b2633f
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2022-04-28 11:18:47 -0400

    add more og tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 /* eslint-disable @next/next/no-document-import-in-page */
 import { Html, Head, Main, NextScript } from 'next/document';
 
@@ -5,12 +6,33 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				<title>Amogus Image Converter</title>
+				<meta name='description' content='Convert images into twerking amogus gifs!' />
+
+				<meta property='og:url' content='https://img-convert.igamble.dev/' />
+				<meta property='og:type' content='website' />
 				<meta property='og:title' content='Amogus Image Converter' />
 				<meta
 					property='og:description'
 					content='Convert images into twerking amogus gifs!'
 				/>
-				<meta property='og:image' content='/example1-32.gif' />
+				<meta
+					property='og:image'
+					content='https://img-convert.igamble.dev/example1-32.gif'
+				/>
+
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta property='twitter:domain' content='img-convert.igamble.dev' />
+				<meta property='twitter:url' content='https://img-convert.igamble.dev/' />
+				<meta name='twitter:title' content='Amogus Image Converter' />
+				<meta
+					name='twitter:description'
+					content='Convert images into twerking amogus gifs!'
+				/>
+				<meta
+					name='twitter:image'
+					content='https://img-convert.igamble.dev/example1-32.gif'
+				/>
 			</Head>
 			<body>
 				<Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Examples from '../components/Examples';
 import ImageConverter from '../components/ImageConverter';
 import styles from '../styles/Home.module.css';
@@ -9,9 +8,6 @@ const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
 			<main className={styles.main}>
-				<Head>
-					<title>Amogus Image Converter</title>
-				</Head>
 				<ImageConverter />
 				<div>
 					<Link href='/about'>
